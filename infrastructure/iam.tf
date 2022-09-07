438888dd710446a0612847502b7d7d29357f92bd
Vinicius Cunha <vinicius.cunha@ifood.com.br>
2022-09-07 19:48:03 -0300

    .

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -60,10 +60,8 @@ resource "aws_iam_policy" "lambda" {
         },
         {
           "Action": "iam:PassRole",
-          "Resource": ["arn:aws:iam::127012818163:role/EMR_DefaultRole",
-                       "arn:aws:iam::127012818163:role/EMR_EC2_DefaultRole",
-                       "arn:aws:sts::386835512844:assumed-role/XPLambdaRole/ExecuteEMR",
-                       "arn:aws:iam::386835512844:role/EMR_DefaultRole"],
+          "Resource": ["arn:aws:iam::386835512844:role/EMR_DefaultRole",
+                       "arn:aws:iam::386835512844:role/EMR_EC2_DefaultRole"],
           "Effect": "Allow"
         }
     ]
