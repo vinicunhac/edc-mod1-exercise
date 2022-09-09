e8487a92fb183bebb070a905794000f21a531f62
Vinicius Cunha <vinicius.cunha@ifood.com.br>
2022-09-08 23:08:56 -0300

    subindo airflow

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -1,20 +1,20 @@
 
-resource "aws_lambda_function" "executa_emr" {
-  filename      = "lambda_function_payload.zip"
-  function_name = var.lambda_function_name
-  role          = aws_iam_role.lambda.arn
-  handler       = "lambda_function.handler"
-  memory_size   = 128
-  timeout       = 30
+# resource "aws_lambda_function" "executa_emr" {
+#   filename      = "lambda_function_payload.zip"
+#   function_name = var.lambda_function_name
+#   role          = aws_iam_role.lambda.arn
+#   handler       = "lambda_function.handler"
+#   memory_size   = 128
+#   timeout       = 30
 
-  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+#   source_code_hash = filebase64sha256("lambda_function_payload.zip")
 
-  runtime = "python3.8"
+#   runtime = "python3.8"
 
-  tags = {
-    IES   = "XP"
-    CURSO = "EDC"
-  }
+#   tags = {
+#     IES   = "XP"
+#     CURSO = "EDC"
+#   }
 
-}
-####
+# }
+# ####
