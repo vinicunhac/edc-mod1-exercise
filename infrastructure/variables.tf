bed1688dd77daff58a4a800ba7c0774e10e59cd8
Vinicius Cunha <vinicius.cunha@ifood.com.br>
2022-09-08 22:59:13 -0300

    subindo airflow

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -7,3 +7,10 @@ variable "lambda_function_name" {
   default     = "ExecuteEMR"
 }
 
+variable "airflow_subnet_id" {
+  default = "subnet-038c9366456ffe5d7"
+}
+
+variable "vpc_id" {
+  default = "vpc-004b8a904b3f7c30f"
+}
